7bd7d46927afafd6a68755f8657e2560de33d757
stiv-rs <strodriguezna@gmail.com>
2024-11-14 18:23:17 -0500

    Actualizar la cantidad del producto

--- src/app/components/car-app.component.ts
+++ src/app/components/car-app.component.ts
@@ -24,7 +24,18 @@ export class CarAppComponent implements OnInit{
   }
 
   onAddCart(product: Product){
-    this.items = [...this.items, {product: {...product}, quantity: 1}];
+    const hasItem = this.items.find(item => item.product.id === product.id );
+    if (hasItem) {
+     this.items = this.items.map(item => {
+       if (item.product.id === product.id) {
+        return {
+          ...item, quantity: item.quantity + 1
+        }
+      }
+      return item;
+     })  
+    }else{
+      this.items = [...this.items, {product: {...product}, quantity: 1}];
+    }
   }
-
 }
